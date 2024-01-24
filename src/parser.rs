da08c7a7652e07a888489334dd1cd168910ced1a
Johnnei <jtiesinga@gmail.com>
2024-01-24 21:22:43 +0700

    Add whitespace parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,11 @@
 use std::collections::HashMap;
 
-use nom::{IResult, branch::alt, bytes::complete::tag, combinator::value};
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_while},
+    combinator::value,
+    IResult,
+};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum HoconValue {
@@ -23,11 +28,27 @@ fn boolean<'a>(input: &'a str) -> IResult<&'a str, HoconValue> {
     alt((parse_true, parse_false))(input)
 }
 
+fn whitespace<'a>(input: &'a str) -> IResult<&'a str, ()> {
+    let (input, _) = take_while(|c: char| {
+        c.is_whitespace()
+            || c == '\t'
+            || c == '\n'
+            || c == '\u{000B}'
+            || c == '\u{000C}'
+            || c == '\r'
+            || c == '\u{001C}'
+            || c == '\u{001D}'
+            || c == '\u{001E}'
+            || c == '\u{001F}'
+    })(input)?;
+    Ok((input, ()))
+}
+
 #[cfg(test)]
 mod tests {
 
-    use crate::parser::HoconValue;
     use super::*;
+    use crate::parser::HoconValue;
 
     #[test]
     fn test_null() {
@@ -44,4 +65,8 @@ mod tests {
         assert_eq!(boolean("false"), Ok(("", HoconValue::HoconBoolean(false))));
     }
 
+    #[test]
+    fn test_whitespace() {
+        assert_eq!(whitespace("     test"), Ok(("test", ())));
+    }
 }
